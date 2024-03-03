3d62fc3183b42139d568d2c81b0bc79cc3c2f32c
patouri <78543418+patouri@users.noreply.github.com>
2024-03-03 09:25:53 +0200

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,18 @@
 class Main {
   public static void main(String[] args) {
-    System.out.println("Hello World!");
+    public static String bikers(Queue<biker> q,int cc){
+    String id;
+    q.insert(null);
+    while(cc > q.head().getcc()){
+        q.insert(q.remove());
+    }
+    id = q.head().getId();
+    q.remove();
+    while(q.head()!= null){
+        q.insert(q.remove());
+    }
+    q.remove();
+    return id;
+}
   }
 }
