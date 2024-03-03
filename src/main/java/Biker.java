feabdf5a8934c97d553dab51b4b937d5bdea052e
patouri <78543418+patouri@users.noreply.github.com>
2024-03-03 09:27:38 +0200

    Create Biker.java

---
+++ src/main/java/Biker.java
@@ -0,0 +1,28 @@
+public class Biker {
+
+private String id;
+private int cc;
+
+public Biker(String id,int cc) {
+this.id=id;
+this.cc=cc;
+}
+
+public String getId() {
+return id;
+}
+
+public void setId(String id) {
+this.id = id;
+}
+
+public int getcc() {
+return cc;
+}
+
+public void setcc(int cc) {
+this.cc = cc;
+}
+
+
+}
